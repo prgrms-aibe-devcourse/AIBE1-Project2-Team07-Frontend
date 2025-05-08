4775ad2c82e9a4f7bdc90ddb20bafba9473933ce
김주엽 <kjyy08@naver.com>
2025-05-08 21:59:03 +0900

    refactor: 프로필 관련 라우터 개선

--- routes/profile.js
+++ routes/profile.js
@@ -1,17 +1,22 @@
 var express = require('express');
 var router = express.Router();
+const path = require("path");
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  if (!req.user) {
-    return res.redirect('/login');
-  }
+router.get('/', function (req, res, next) {
+    const user = JSON.parse(req.cookies.user);
 
-  if (req.user.role === 'trainer') {
-    return res.redirect(`/trainer-profile.html?id=${req.user.id}`);
-  } else {
-    return res.redirect(`/user-profile.html?id=${req.user.id}`);
-  }
+    if (user === null) {
+        return res.redirect('/');
+    }
+
+    if (user.userRole === 'TRAINER') {
+        res.sendFile(path.join(__dirname, '../public/trainer-profile.html'));
+        // return res.redirect(`/trainer-profile.html?id=${req.user.id}`);
+    } else {
+        res.sendFile(path.join(__dirname, '../public/user-mypage.html'));
+        // return res.redirect(`/user-profile.html?id=${req.user.id}`);
+    }
 });
 
 module.exports = router;
