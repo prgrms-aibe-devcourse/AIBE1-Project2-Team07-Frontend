43d95b0c2b30545acc4e4864cdf61adfbe657c10
soheeGit <63wlsthgml@gmail.com>
2025-05-12 02:28:43 +0900

    수락, 거절 완료

--- routes/index.js
+++ routes/index.js
@@ -4,24 +4,16 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    // 헤더 설정
-    res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
-    res.setHeader('Pragma', 'no-cache');
-    res.setHeader('Expires', '0');
-
-    // Content-Length 헤더 제거 (필요시)
-    res.removeHeader('Content-Length');
-
     let user;
     try {
         user = JSON.parse(req.cookies.user);
     } catch (e) {
         console.error('사용자 쿠키 파싱 오류:', e);
-        return res.redirect('/login.html'); // 로그인 페이지로 리다이렉트
+        return res.sendFile(path.join(__dirname, '../public/home.html'));
     }
 
     // 사용자 역할에 따라 다른 페이지 제공
-    if (user && user.userRole === 'ADMIN') {
+    if (user.userRole === 'ADMIN') {
         return res.sendFile(path.join(__dirname, '../public/cert.html'));
     } else {
         return res.sendFile(path.join(__dirname, '../public/home.html'));
